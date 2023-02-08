59a34c0aaf07ffbcce872d12450d3a7d1d6e472a
gerincon57 <geraldin.rincon@mercadolibre.com.co>
2023-02-08 13:22:18 -0500

    inicio testHandler

--- internal/products/handler_test.go
+++ internal/products/handler_test.go
@@ -1,17 +1,20 @@
 package products
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
-func createServerProducts(st Repository) *gin.Engine {
+func createServerProducts() *gin.Engine {
 
-	rp := NewStorageMock(st)
+	rp := NewStorageMock()
 	sv := NewServiceLocal(rp)
 
 	//server
@@ -20,9 +23,9 @@ func createServerProducts(st Repository) *gin.Engine {
 	//router
 	router := server.Group("/api/v1")
 	{
-		//h := NewControllerMovie(sv)
-		group := router.Group("/movies")
-		group.POST("", h.Create())
+		h := NewHandler(sv)
+		group := router.Group("/products")
+		group.GET("", h.GetProducts)
 
 	}
 
@@ -46,7 +49,31 @@ var (
 	ErrMock = errors.New("internal error")
 )
 
-type responseMovie struct {
-	Message string  `json:"message"`
-	Data    Product `json:"data"`
+func TestGetProducts(t *testing.T) {
+	// arrange
+	server := createServerProducts()
+
+	// act
+	t.Run("Happy Path", func(t *testing.T) {
+		// arrange
+		req, res := NewRequest(http.MethodGet, "/api/v1/products?seller_id='mock'", "")
+
+		expectedProd := Product{
+			ID:          "mock",
+			SellerID:    "FEX112AC",
+			Description: "generic product",
+			Price:       30.25,
+		}
+
+		var expectedProdSlice = []Product{expectedProd}
+		// act
+		server.ServeHTTP(res, req)
+		var r []Product
+		err := json.Unmarshal(res.Body.Bytes(), &r)
+
+		// assert
+		assert.NoError(t, err)
+		assert.Equal(t, 200, res.Code)
+		assert.Equal(t, expectedProdSlice, r)
+	})
 }
